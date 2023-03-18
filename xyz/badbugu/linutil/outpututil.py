f4ed6a7b74eded679d3e7b37dcea34fafdf99451
badbugu17 <sun656105944@outlook.com>
2023-03-18 23:45:07 +0800

    添加文件

---
+++ xyz/badbugu/linutil/outpututil.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# DateTime  : 2023年3月12日18:04:58
+# Author    : Badbugu17
+# File      : outpututil.py
+# Software  : Visual Studio Code
+
+
+# 为了解决控制台输出格式乱掉, 一个中文的宽度是两个英文或数字的宽度
+def output_as_table(title, data) -> None:
+    '''
+    title: list类型 标题
+    data: 二维数组 数据
+    '''
+    
